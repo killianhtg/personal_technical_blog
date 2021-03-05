0a716e2c610b310618112f0dd510ed7900a407d7
--local <tiangehe@email.arizona.edu>
2021-03-05 01:45:22 -0800

    set up package and depandencies

---
+++ db/DBController.js
@@ -0,0 +1,10 @@
+const { MongoClient } = require("mongodb");
+
+function DBController() {
+  const dbController = {};
+  // TODO: implement connection and function to database
+
+  return dbController;
+}
+
+module.exports = DBController();
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const DBController = require("../db/DBController.js");
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
