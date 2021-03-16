02d99eedfa41748044c98f57df093ce8bd42f214
ningd <55850050+ningowo@users.noreply.github.com>
2021-03-16 14:27:49 -0700

    Update DBController.js

--- db/DBController.js
+++ db/DBController.js
@@ -9,12 +9,23 @@ function DBController() {
   const DB_NAME = "personalTecBlogDB";
 
   dbController.createBlog = async (blog) => {
-    await MongoPool.getInstance(function (client) {
-      const blogsCol = client.db(DB_NAME).collection("blogs");
-      const res = blogsCol.insertOne(blog);
+    let client;
+    try {
+      client = new MongoClient(url, { useUnifiedTopology: true });
+      console.log("Connecting to the db");
+      await client.connect();
+      console.log("Connected!");
+      const db = client.db(DB_NAME);
+      const blogCol = db.collection("blogs");
+      console.log("Collection ready, insert ", blog);
+      const res = await blogCol.insertOne(blog);
       console.log("Inserted", res);
+
       return res;
-    });
+    } finally {
+      console.log("Closing the connection");
+      client.close();
+    }
   };
   
   dbController.getBlogs = async (query = {}) => {
