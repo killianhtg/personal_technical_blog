aa1be8bd5f39be46abeb3b902d3a8050583c672d
ningd <55850050+ningowo@users.noreply.github.com>
2021-03-16 13:29:13 -0700

    Update DBController.js

--- db/DBController.js
+++ db/DBController.js
@@ -15,40 +15,8 @@ function DBController() {
       console.log("Inserted", res);
       return res;
     });
-
-    // let client;
-    // try {
-    //   client = new MongoClient(url, { useUnifiedTopology: true });
-    //   console.log("Connecting to the db");
-    //   await client.connect();
-    //   console.log("Connected!");
-    //   const db = client.db(DB_NAME);
-    //   const blogCol = db.collection("blogs");
-    //   console.log("Collection ready, insert ", blog);
-    //   const res = await blogCol.insertOne(blog);
-    //   console.log("Inserted", res);
-
-    //   return res;
-    // } finally {
-    //   console.log("Closing the connection");
-    //   client.close();
-    // }
   };
-
-  // dbController.getBlogs = async function () {
-  //   var blogs;
-  //   await MongoPool.getInstance(function (client) {
-  //     //console.log("==============================");
-
-  //     const blogsCol = client.db(DB_NAME).collection("blogs");
-  //     console.log("Blogs Collection ready, insert ", blogsCol);
-
-  //     blogs = blogsCol.find({}).toArray();
-  //     console.log("Got blogs");
-  //   });
-  //   return blogs;
-  // };
-
+  
   dbController.getBlogs = async (query = {}) => {
     let client;
     try {
@@ -112,13 +80,6 @@ function DBController() {
   };
 
   dbController.getUser = async function (name) {
-    // var pwd;
-    // await MongoPool.getInstance(function (client) {
-    //   const users = client.db(DB_NAME).collection("admin");
-    //   pwd = users.find({ username: name }).toArray();
-    // });
-    // return pwd;
-
     let client;
     try {
       client = new MongoClient(url, { useUnifiedTopology: true });
@@ -129,8 +90,7 @@ function DBController() {
       const usersCol = db.collection("admin");
       console.log("Collection ready, get users ", name);
       const res = await usersCol.find({ username: name }).toArray();
-      console.log("======================================name");
-
+      //console.log("======================================name");
       return res;
     } catch (err) {
       console.log("dbcontroller error", err);
